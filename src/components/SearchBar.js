30f64f4104586bd1a3efe707c7a9f2807e7dfcb3
adammak <amine.dammak@habemus.com>
2021-10-06 10:54:54 +0100

    Created the seach component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const SearchBar = ({ term, onTermChange }) => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle} color="black" />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        onChangeText={(enteredText) => onTermChange(enteredText)}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    marginTop: 15,
+    backgroundColor: "rgb(204, 204, 204)",
+    height: 50,
+    marginHorizontal: 15,
+    borderRadius: 5,
+    flexDirection: "row",
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15,
+  },
+});
