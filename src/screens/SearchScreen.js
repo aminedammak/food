0944286b98ada0c0cf552f97161c912e1200c9ce
adammak <amine.dammak@habemus.com>
2021-10-06 13:59:00 +0100

    Catch the errors in sending a request to the api

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,21 +6,27 @@ import yelp from "../apis/yelp";
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const makeSearch = async () => {
-    const response = await yelp.get("/search", {
-      params: {
-        term,
-        limit: 44,
-        location: "san jose",
-      },
-    });
-    setResults(response.data.businesses);
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          term,
+          limit: 44,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage("Something went wrong!");
+    }
   };
 
   return (
     <View>
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={makeSearch} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>
         Search {term}: {results.length}
       </Text>
